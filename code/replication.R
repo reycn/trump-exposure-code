6d9d5fb92678e55c11fb713eec7932685111d5de
Rongxin <rongxin@u.nus.edu>
2024-10-17 16:50:23 +0800

    Add all platforms

--- code/replication.R
+++ code/replication.R
@@ -1,8 +1,6 @@
 # R version: 4.4.1 (2024-06-14)
 
-# 1. Load packages
-
-
+# 1. Load packages ########################################
 if (!is.element('glue', installed.packages()[,1])) {
     install.packages('glue',repos = "http://cran.us.r-project.org")
 }
@@ -16,14 +14,14 @@ library(glue)
 library(lfe)
 library(dplyr)
 
-# 2. Define parameters
-platforms <- c("instagram") # We have three platforms
+# 2. Define parameters ####################################
+platforms <- c("facebook", "instagram", "twitter", "three") # We have three platforms
 dvs <- c("exposure", "engagement") # We have two DVs: (1) media coverage and (2) audience engagement
 eq <- as.formula("log1p(y) ~ g*s + g*l + log1p(pres) + log1p(gop)") # Our regression model, log1p for possible zeroes
 
-# 3. For loop, 2x4 = 8 replications
-for (platform in platforms) {
-    for (dv in dvs) {
+# 3. For loop, 2x4 = 8 replications ########################
+for (dv in dvs){
+    for  (platform in platforms) {
         print(glue("============= Replicating: {if_else(dv == 'exposure', 'Media Coverage', 'Audience Engagement')} / {tools::toTitleCase(platform)} ============="))
         # 3.1 Load the data
         path <- glue("../data/{platform}.{dv}.csv") # Path to the data
